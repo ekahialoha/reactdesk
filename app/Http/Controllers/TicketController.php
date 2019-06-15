a915c97990c9888bf0eb0e047e77ec1fe383aebe
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-15 13:20:57 -1000

    Able to create ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Http\Resources\Ticket as TicketResource;
+use App\Traits\CreateTrackId;
 use App\Ticket;
+use \stdClass;
+
+
 
 class TicketController extends Controller
 {
+    use CreateTrackId;
 
     public function index()
     {
@@ -24,7 +30,22 @@ class TicketController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $ticket = new Ticket();
+        $ticket->track_id = $this->generateTrackId();
+        $ticket->department_id = $request->input('department_id');
+        $ticket->name = $request->input('name');
+        $ticket->email = $request->input('email');
+        $ticket->subject = $request->input('subject');
+        $ticket->message = $request->input('message');
+        $ticket->priority = $request->input('priority');
+        $ticket->save();
+
+        $return = [
+            'status' => 201,
+            'data' => new TicketResource($ticket)
+        ];
+
+        return response()->json($return, $return['status']);
     }
 
 
