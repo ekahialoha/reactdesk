f3d213ab937f9eab9250df95a25efd31bf1a5b0c
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-15 19:07:00 -1000

    start of react router list of departments start

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
+
+import DepartmentList from './DepartmentList.js';
 
 class App extends Component {
     render() {
         return (
-            <div>ReactDesk</div>
+            <React.Fragment>
+                <div>ReactDesk</div>
+                <Switch>
+                    <Redirect exact from="/" to ="/ticket/new" />
+                    <Route exact path="/ticket/new" component={DepartmentList} />
+                </Switch>
+            </React.Fragment>
         );
     }
 }
