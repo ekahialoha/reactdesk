f62e56f58330816d1fa2256532e1b3ecc179eead
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-15 16:13:12 -1000

    able to see all replies

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+// use App\Http\Resources\Reply as ReplyResource;
+use App\Reply;
+use \stdClass;
 
 class ReplyController extends Controller
 {
     public function index()
     {
-        //
+        $replies = Reply::all();
+
+        $return = [
+                'status' => 200,
+                'data' => $replies
+        ];
+
+        return response()->json($return, $return['status']);
     }
 
     public function store(Request $request)
