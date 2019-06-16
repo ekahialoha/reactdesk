b2494db54c404cc75c786ba6fc3acd6da32a1914
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-15 16:27:52 -1000

    Added functionality to create reply

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-// use App\Http\Resources\Reply as ReplyResource;
+use App\Http\Resources\Reply as ReplyResource;
 use App\Reply;
 use \stdClass;
 
@@ -16,7 +16,7 @@ class ReplyController extends Controller
 
         $return = [
                 'status' => 200,
-                'data' => $replies
+                'data' => ReplyResource::collection($replies)
         ];
 
         return response()->json($return, $return['status']);
@@ -24,7 +24,19 @@ class ReplyController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $reply = new Reply();
+        $reply->ticket_id = $request->input('ticket_id');
+        $reply->user_id = $request->input('user_id');
+        $reply->name = $request->input('name');
+        $reply->message = $request->input('message');
+        $reply->save();
+
+        $return = [
+            'status' => 201,
+            'data' => new ReplyResource($reply)
+        ];
+
+        return response()->json($return, $return['status']);
     }
 
     public function show($id)
