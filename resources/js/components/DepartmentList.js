28b821a318dbe06c17cbc949c6b7fbc96520d935
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-15 19:10:59 -1000

    created state and pulling api deparment index

--- resources/js/components/DepartmentList.js
+++ resources/js/components/DepartmentList.js
@@ -5,6 +5,26 @@ class DepartmentList extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            'departments': []
+        };
+    }
+
+    fetchDepartments = () => {
+        axios.get('/api/departments')
+        .then(res => {
+            this.setState({
+                'departments': res.data
+            }, () => {
+                console.log(this.state.departments);
+            });
+        }).catch(err => {
+            console.log('DepartmentList.fetchDepartments', err);
+        });
+    }
+
+    componentDidMount = () => {
+        this.fetchDepartments();
     }
 
     render() {
