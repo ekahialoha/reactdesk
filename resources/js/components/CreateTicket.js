4aacc414f17ff9eb156a2fe3843950a4e8f101c8
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-15 19:32:50 -1000

    basic html form structure

--- resources/js/components/CreateTicket.js
+++ resources/js/components/CreateTicket.js
@@ -9,6 +9,40 @@ class CreateTicket extends Component {
     render() {
         return (
             <div>
+                <h2>Create Ticket - {this.state.department.name}</h2>
+                <form>
+                    <input required
+                            type="text"
+                            placeholder="Name"
+                            id="name"
+                    />
+                    <input required
+                            type="email"
+                            placeholder="Email"
+                            id="email"
+                    />
+                    <input required
+                            type="text"
+                            placeholder="Subject"
+                            id="subject"
+                    />
+                    <textarea
+                            required
+                            placeholder="Message"
+                            id="message"
+                    ></textarea>
+                    <select
+                            required
+                            id="priority"
+                    >
+                        <option value="1">Normal</option>
+                        <option value="2">High</option>
+                        <option value="3">Urgent</option>
+                    </select>
+
+                    <button>Create Ticket</button>
+                </form>
+
 
             </div>
         );
