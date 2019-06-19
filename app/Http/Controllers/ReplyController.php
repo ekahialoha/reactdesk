a5a9c76f2d720cd278cf16327df6eb4568c0e98b
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-18 19:27:14 -1000

    Moddileware/Auth for replies

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Http\Resources\Reply as ReplyResource;
+use App\Ticket;
 use App\Reply;
 use \stdClass;
 
 class ReplyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('store');
+    }
+
     public function index()
     {
         $replies = Reply::all();
@@ -24,22 +30,42 @@ class ReplyController extends Controller
 
     public function store(Request $request)
     {
-        $reply = new Reply();
-        $reply->ticket_id = $request->input('ticket_id');
-        $reply->user_id = $request->input('user_id');
-        $reply->name = $request->input('name');
-        $reply->message = $request->input('message');
-        $reply->save();
+        try {
+            $ticket_id = $request->input('ticket_id');
 
-        $return = [
-            'status' => 201,
-            'data' => new ReplyResource($reply)
-        ];
+            if ($request->cookie('token') && JWTAuth::parseToken()->authenticate()) {
+                $ticket = Ticket::findOrFail($id);
+            } else {
+                $ticket = Ticket::where('track_id', $ticket_id)->firstOrFail();
+                $ticket_id = $ticket->id;
+            }
+
+            $reply = new Reply();
+            $reply->ticket_id = $ticket_id;
+            $reply->user_id = $request->input('user_id');
+            $reply->name = $request->input('name');
+            $reply->message = $request->input('message');
+            $reply->save();
+
+            $return = [
+                'status' => 201,
+                'data' => new ReplyResource($reply)
+            ];
+        } catch (ModelNotFoundException $e) {
+            $data = new stdClass();
+            $data->error = 'Ticket Not Found';
+            $ticket_id = $request->input('ticket_id');
+
+            $return = [
+                'status' => 404,
+                'data' => $request->input('ticket_id')
+            ];
+        }
 
         return response()->json($return['data'], $return['status']);
     }
 
-    public function show($id)
+    public function show($id, Request $request)
     {
         try {
             $reply = Reply::findOrFail($id);
