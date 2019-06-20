44b67fd0bbb1b75bf98d370746bc53b9f14ae89d
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-20 11:59:58 -1000

    Simple form with basic state for reply

--- resources/js/components/NewReply.js
+++ resources/js/components/NewReply.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Form, Button, Card } from 'react-bootstrap';
 
 class NewReply extends Component {
     constructor(props) {
@@ -11,17 +12,41 @@ class NewReply extends Component {
     }
 
     handleChanges = (e) => {
-
+        this.setState({
+            [e.target.id]: e.target.value
+        });
     }
 
     handleSubmit = (e) => {
+        e.preventDefault();
 
+        console.log(this.state.message);
+
+        this.setState({
+            name: '',
+            showForm: false
+        });
     }
 
     render() {
         return (
             <React.Fragment>
-                
+                <Card>
+                    <Card.Header>Reply</Card.Header>
+                    <Card.Body>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Control
+                                required
+                                as="textarea"
+                                value={this.state.message}
+                                placeholder="Enter your response here"
+                                id="message"
+                                onChange={this.handleChanges}
+                            />
+                            <Button type="submit">Reply</Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
             </React.Fragment>
         );
     }
