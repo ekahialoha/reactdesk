401206aa4820fedd4a08ae70698ecdef679a2ddc
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-21 15:14:06 -1000

    Added list header

--- resources/js/components/staff/Dashboard.js
+++ resources/js/components/staff/Dashboard.js
@@ -52,6 +52,19 @@ class Dashboard extends Component {
         }
     }
 
+    fetchTickets = () => {
+        axios.get('/api/tickets')
+        .then((res) => {
+            console.log(res);
+        }).catch((err) => {
+            console.log('Staff/Dashboard.fetchTickets', err);
+        })
+    }
+
+    componentDidMount = () => {
+        this.fetchTickets();
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -131,6 +144,13 @@ class Dashboard extends Component {
                             <Route /*path="/staff/tickets/open"*/ render={() => <h1>Open</h1>} />
                         </Switch>
                         <div>
+                            <div className="ticket-row ticket-row-heading">
+                                <span className="ticket-check"></span>
+                                <span className="ticket-id">Ticket ID</span>
+                                <span className="ticket-subject">Subject</span>
+                                <span className="ticket-name">Customer</span>
+                                <span className="ticket-status">Status</span>
+                            </div>
                             <div className="ticket-row">
                                 <span className="ticket-check"><i className="far fa-envelope"></i></span>
                                 <span className="ticket-id">7489-MJB-166</span>
@@ -153,15 +173,6 @@ class Dashboard extends Component {
                                 <span className="ticket-status">Status</span>
                             </div>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius sapien nulla, at consequat massa tempor id. Nulla feugiat, ipsum vel fermentum tincidunt, purus diam posuere enim, at lobortis diam libero ut ex. Maecenas eu erat in dui pharetra mattis. Quisque dolor purus, bibendum vel elementum quis, ullamcorper sit amet nisl. Suspendisse eget dapibus lectus. In aliquam velit et orci pretium, sed posuere ex sodales. Sed dictum quam vitae neque tempor mattis. Suspendisse sit amet luctus purus.</p>
-
-                        <p>Nullam ac nulla nisl. Sed odio velit, sodales vitae sem et, pellentesque aliquam quam. In bibendum id nunc in semper. Donec placerat magna in porta dapibus. Duis imperdiet diam eu nulla maximus, in iaculis arcu porta. Morbi faucibus tincidunt elit, consectetur molestie sapien sodales sed. Vestibulum a ex aliquet, vulputate libero vel, commodo diam. Fusce in sodales tellus. Etiam vitae blandit ex. Suspendisse libero ex, lobortis ut suscipit sit amet, finibus a arcu. Donec id nisi varius, suscipit sapien nec, congue lorem. Pellentesque faucibus suscipit leo. Aenean sed neque eu neque ornare bibendum. Cras urna ipsum, consequat ac tortor eget, iaculis volutpat tortor.</p>
-
-                        <p>Aenean congue, felis in lacinia dapibus, velit nibh vulputate justo, quis imperdiet lacus dui mattis ex. Vivamus nisi ex, placerat nec erat nec, suscipit lobortis neque. Donec sodales bibendum posuere. Proin eu consequat elit. Mauris pharetra ullamcorper lectus, et laoreet elit. In aliquet in mi ac sodales. Praesent maximus purus ex, faucibus ultrices dui eleifend hendrerit. Duis volutpat mauris in felis fermentum efficitur. Nam fermentum auctor nisi, vel pulvinar quam mollis quis. Donec non eros quis libero hendrerit pharetra vel eu turpis. Sed vel erat a lacus ultricies dictum sit amet eget ligula. Aliquam metus est, sodales vulputate nisi suscipit, eleifend tempor lacus. Etiam rhoncus, dolor ut venenatis dapibus, libero justo consequat lorem, at auctor tellus augue eget risus. Maecenas dignissim felis et ornare ullamcorper.</p>
-
-                        <p>Praesent euismod a velit et tincidunt. Maecenas est augue, dapibus ullamcorper facilisis convallis, vehicula quis diam. Phasellus et nibh vitae risus auctor rhoncus at nec nibh. Donec accumsan ipsum vitae odio volutpat malesuada. Sed in nisi non sem sollicitudin suscipit non at sapien. Vivamus massa risus, posuere id feugiat eu, blandit et leo. Aliquam nec sem viverra, feugiat lorem quis, suscipit lectus. In ac suscipit eros. Donec bibendum, metus non ultricies egestas, tortor ligula finibus ipsum, in hendrerit mi orci id neque. Donec at eros ac purus mollis maximus. Aliquam erat volutpat.</p>
-
-                        <p>Nunc consectetur consequat hendrerit. Fusce fringilla diam massa. In euismod sollicitudin tellus, ac lobortis ex mollis vitae. Nam orci augue, posuere in leo quis, pharetra maximus mi. Duis accumsan tortor sit amet turpis feugiat sodales. Aenean id malesuada nulla. Donec lectus est, consectetur eget tempor eu, porttitor aliquam lectus. Nam faucibus felis sed erat feugiat commodo. Mauris laoreet orci sit amet sodales lobortis. Duis posuere semper placerat. Aliquam vitae posuere lorem, eu rutrum mi. Suspendisse potenti. In est nulla, ullamcorper quis leo at, pulvinar gravida ante. Maecenas finibus ullamcorper porttitor. Aliquam in velit lacinia, finibus ante ac, dictum nisl.</p>
                     </main>
                 </Container>
                 <Footer unsticky />
