0104cd1ce3a21021dae3b028c0799f4cc1a47018
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-21 18:14:03 -1000

    Listing departments in staff interface

--- resources/js/components/staff/Departments.js
+++ resources/js/components/staff/Departments.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import { ListGroup } from 'react-bootstrap';
 
 class Departments extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            departments: []
+        };
     }
 
     fetchDepartments = () => {
         axios.get('/api/departments')
         .then((res) => {
             console.log(res);
+            this.setState({
+                departments: res.data
+            });
         }).catch((err) => {
             console.log('Staff/Departments.fetchDepartments', err);
         })
@@ -22,6 +30,19 @@ class Departments extends Component {
         return (
             <React.Fragment>
                 <h1>Departments</h1>
+                <ListGroup>
+                    {this.state.departments.map((department) => {
+                        return (
+                            <ListGroup.Item key={department.id}>
+                                {department.name}
+                                <span>
+                                    <i className="fas fa-edit"></i>
+                                    <i className="fas fa-trash-alt"></i>
+                                </span>
+                            </ListGroup.Item>
+                        );
+                    })}
+                </ListGroup>
             </React.Fragment>
         );
     }
