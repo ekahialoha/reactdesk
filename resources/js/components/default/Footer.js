c315e60e3bf67d0a8bfa1120c6f6b67be6b5e916
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-21 14:11:09 -1000

    Option to reuse footer with unsticky option

--- resources/js/components/default/Footer.js
+++ resources/js/components/default/Footer.js
@@ -8,13 +8,19 @@ class Footer extends Component {
 
     render() {
         return (
-            <footer className="footer fixed-bottom">
-            Copyright 2019 ReactDesk / <a href="https://www.chriskelsom.com" target="_blank">Christian Kelsom-Martin</a>
-                <div>
-                    <a href="https://www.linkedin.com/in/ckelsom-martin/" target="_blank"><i className="fab fa-linkedin"></i></a>
-                    <a href="https://github.com/ekahialoha" target="_blank"><i className="fab fa-github-square"></i></a>
-                </div>
-            </footer>
+            <React.Fragment>
+                {!this.props.unsticky ?
+                     <div className="fixed-bottom-space"></div> :
+                     ''
+                }
+                <footer className={!this.props.unsticky ? 'footer fixed-bottom' : 'footer'}>
+                Copyright 2019 ReactDesk / <a href="https://www.chriskelsom.com" target="_blank">Christian Kelsom-Martin</a>
+                    <div>
+                        <a href="https://www.linkedin.com/in/ckelsom-martin/" target="_blank"><i className="fab fa-linkedin"></i></a>
+                        <a href="https://github.com/ekahialoha" target="_blank"><i className="fab fa-github-square"></i></a>
+                    </div>
+                </footer>
+            </React.Fragment>
         );
     }
 }
