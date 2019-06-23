002dee8b3a1050d277620bd45e1c032c26d27a5f
Christian Kelsom-Martin <44541376+ekahialoha@users.noreply.github.com>
2019-06-22 17:39:14 -1000

    Fix some stying

--- resources/js/components/staff/TicketList.js
+++ resources/js/components/staff/TicketList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
 
 class TicketList extends Component {
     constructor(props) {
@@ -16,7 +17,6 @@ class TicketList extends Component {
         if ('status' in this.props) {
             fetchUrl += `?status=${this.props.status}`
         }
-        console.log(fetchUrl);
         axios.get(fetchUrl)
         .then((res) => {
             console.log(res);
@@ -42,35 +42,39 @@ class TicketList extends Component {
                     onClick={this.fetchTickets}
                 ></i>
             </h1>
-            <div>
-                <div className="ticket-row ticket-row-heading">
-                    <span className="ticket-check"></span>
-                    <span className="ticket-id">Ticket ID</span>
-                    <span className="ticket-subject">Subject</span>
-                    <span className="ticket-name">Customer</span>
-                    <span className="ticket-department">Department</span>
-                    <span className="ticket-status">Status</span>
+            <Card>
+                <Card.Body>
+                <div>
+                    <div className="ticket-row ticket-row-heading">
+                        <span className="ticket-check"></span>
+                        <span className="ticket-id">Ticket ID</span>
+                        <span className="ticket-subject">Subject</span>
+                        <span className="ticket-name">Customer</span>
+                        <span className="ticket-department">Department</span>
+                        <span className="ticket-status">Status</span>
+                    </div>
+                    {this.state.tickets.map((ticket, index) => {
+                        return (
+                            <div
+                                className="ticket-row"
+                                key={index}
+                                id={ticket.id}
+                                onClick={() => {
+                                    this.props.history.push(`/staff/tickets/${ticket.id}`);
+                                }}
+                            >
+                                <span className="ticket-check"><i className="far fa-envelope"></i></span>
+                                <span className="ticket-id">{ticket.track_id}</span>
+                                <span className="ticket-subject">{ticket.subject}</span>
+                                <span className="ticket-name">{ticket.name}</span>
+                                <span className="ticket-department">{ticket.department.name}</span>
+                                <span className="ticket-status">{ticketStatus[ticket.status]}</span>
+                            </div>
+                        );
+                    })}
                 </div>
-                {this.state.tickets.map((ticket, index) => {
-                    return (
-                        <div
-                            className="ticket-row"
-                            key={index}
-                            id={ticket.id}
-                            onClick={() => {
-                                this.props.history.push(`/staff/tickets/${ticket.id}`);
-                            }}
-                        >
-                            <span className="ticket-check"><i className="far fa-envelope"></i></span>
-                            <span className="ticket-id">{ticket.track_id}</span>
-                            <span className="ticket-subject">{ticket.subject}</span>
-                            <span className="ticket-name">{ticket.name}</span>
-                            <span className="ticket-department">{ticket.department.name}</span>
-                            <span className="ticket-status">{ticketStatus[ticket.status]}</span>
-                        </div>
-                    );
-                })}
-            </div>
+                </Card.Body>
+            </Card>
             </React.Fragment>
         );
     }
